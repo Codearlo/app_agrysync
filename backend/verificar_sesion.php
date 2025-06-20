3d3fb5648bf908b85ee055b21462ab79a1285fcc
genius-cabana <1513661@senati.pe>
2025-06-19 21:50:45 -0500

    Subiendo los archivos

--- backend/verificar_sesion.php
+++ backend/verificar_sesion.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 header('Content-Type: application/json');
-$response = ['logueado' => false];
+$response = ['logueado' => false, 'nombre' => ''];
 if (isset($_SESSION['usuario'])) {
   $response['logueado'] = true;
+  $response['nombre'] = $_SESSION['usuario']['nombre']; // Ajusta según tu estructura de sesión
 }
 echo json_encode($response);
 ?>
